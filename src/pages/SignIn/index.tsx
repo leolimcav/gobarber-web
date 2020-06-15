c43914049eac8be8df6748defd2e7596b332d633
Leonardo <leonardo123k@gmail.com>
2020-06-15 17:29:37 -0300

    finalizado tela de login

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FiLogIn, FiLock, FiMail } from 'react-icons/fi';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+import logo from '../../assets/logo.svg';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logo} alt="GoBarber" />
+
+      <h1>Faça seu logon</h1>
+      <form>
+        <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Entrar</Button>
+
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+      <a href="forgot">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+    <Background />
+  </Container>
+);
+
+export default SignIn;
